c5d6acf0a1256344bb08a809050daa2bb724c1c6
Torsten Werner <torsten.werner@westernacher.com>
2016-07-21 14:37:01 +0200

    use pojo

--- src/main/java/example/jooq/JooqExample.java
+++ src/main/java/example/jooq/JooqExample.java
@@ -1,11 +1,12 @@
 package example.jooq;
 
+import example.jooq.generated.tables.pojos.Author;
 import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static example.jooq.generated.tables.Author.AUTHOR;
 
@@ -20,7 +21,12 @@ public class JooqExample implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        final Result<Record> authors = create.select().from(AUTHOR).fetch();
-        System.out.printf("authors:\n%s\n", authors);
+        final List<Author> authors = create.select().from(AUTHOR).fetchInto(Author.class);
+        for (Author author : authors) {
+            final Integer id = author.getId();
+            final String firstName = author.getFirstName();
+            final String lastName = author.getLastName();
+            System.out.printf("Id: %d, author: %s %s\n", id, firstName, lastName);
+        }
     }
 }
