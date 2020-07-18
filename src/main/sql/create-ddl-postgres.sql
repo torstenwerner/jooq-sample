affb89221199b5a2f823ea28120bd065af0a71e1
Torsten Werner <torsten.werner@westernacher.com>
2020-07-18 19:45:13 +0200

    add README.adoc and fix database initialization

--- src/main/sql/create-ddl-postgres.sql
+++ src/main/sql/create-ddl-postgres.sql
@@ -13,8 +13,8 @@ CREATE TABLE amount
   CONSTRAINT amount_pk PRIMARY KEY (id, customer_id)
 );
 
-INSERT INTO public.amount (id, customer_id, transaction) VALUES (1, 1, 9.99);
-INSERT INTO public.amount (id, customer_id, transaction) VALUES (2, 1, 42.23);
-INSERT INTO public.amount (id, customer_id, transaction) VALUES (3, 1, -5.00);
-INSERT INTO public.amount (id, customer_id, transaction) VALUES (2, 2, 49.99);
-INSERT INTO public.amount (id, customer_id, transaction) VALUES (1, 2, -17.50);
+INSERT INTO amount (id, customer_id, amount) VALUES (1, 1, 9.99);
+INSERT INTO amount (id, customer_id, amount) VALUES (2, 1, 42.23);
+INSERT INTO amount (id, customer_id, amount) VALUES (3, 1, -5.00);
+INSERT INTO amount (id, customer_id, amount) VALUES (2, 2, 49.99);
+INSERT INTO amount (id, customer_id, amount) VALUES (1, 2, -17.50);
