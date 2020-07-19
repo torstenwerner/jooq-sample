83e6fa15e2ae14c4a4681f7dccd1267f559df1b5
Torsten Werner <torsten.werner@westernacher.com>
2020-07-19 10:20:06 +0200

    start adding support for oracle

---
+++ src/main/sql/create-ddl-oracle.sql
@@ -0,0 +1,31 @@
+create table author
+(
+    id         int           not null
+        constraint AUTHOR_PK primary key,
+    first_name VARCHAR2(255) not null,
+    last_name  VARCHAR2(255) not null
+);
+
+create table amount
+(
+    id          int          not null,
+    customer_id int          not null,
+    amount      NUMBER(9, 2) not null,
+    CONSTRAINT amount_pk PRIMARY KEY (id, customer_id)
+);
+
+INSERT INTO amount (id, customer_id, amount)
+VALUES (1, 1, 9.99);
+INSERT INTO amount (id, customer_id, amount)
+VALUES (2, 1, 42.23);
+INSERT INTO amount (id, customer_id, amount)
+VALUES (3, 1, -5.00);
+INSERT INTO amount (id, customer_id, amount)
+VALUES (2, 2, 49.99);
+INSERT INTO amount (id, customer_id, amount)
+VALUES (1, 2, -17.50);
+
+INSERT INTO author (id, first_name, last_name)
+VALUES (1, 'Hildegunst', 'von Mythenmetz');
+INSERT INTO author (id, first_name, last_name)
+VALUES (2, 'Danzelot', 'von Silbendrechsler');
